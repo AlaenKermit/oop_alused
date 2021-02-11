d3828db6b3130d119e77aaa46e00ce098504c27f
Allan Kerme <allan.kerme@khk.ee>
2021-02-11 14:59:39 +0200

    Loodud klass ja tema eksemplaar koos testimisega

---
+++ Ülesanne 4/restoran.py
@@ -0,0 +1,9 @@
+from klass import *
+# loome 1 eksemplaar
+restoran1 = Restoran()
+# määrame eksemplaari atribuutidele konkreetsed väärtused
+restoran1.restorani_nimi = "Hesburger"
+restoran1.soogi_tyyp = "Burgerid"
+# kutsume eksemplaari meetodid tööle
+restoran1.restorani_kirjeldus()
+restoran1.ava_restoran()
