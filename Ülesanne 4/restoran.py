0bb256a7d0ec35b6f262c865e1d2196547880ac9
Allan Kerme <allan.kerme@khk.ee>
2021-02-11 15:03:00 +0200

    Ülesanne 4 lahendus

--- Ülesanne 4/restoran.py
+++ Ülesanne 4/restoran.py
@@ -1,9 +1,24 @@
 from klass import *
-# loome 1 eksemplaar
+# loome 3 eksemplaar
 restoran1 = Restoran()
+restoran2 = Restoran()
+restoran3 = Restoran()
 # määrame eksemplaari atribuutidele konkreetsed väärtused
 restoran1.restorani_nimi = "Hesburger"
 restoran1.soogi_tyyp = "Burgerid"
+### teine restoran
+restoran2.restorani_nimi = "Timmu saiad"
+restoran2.soogi_tyyp = "Saiad"
+### kolmas restoran
+restoran3.restorani_nimi = "Jaanuse köök"
+restoran3.soogi_tyyp = "Kartulipuder hakklihakastmega"
 # kutsume eksemplaari meetodid tööle
 restoran1.restorani_kirjeldus()
 restoran1.ava_restoran()
+### teine restoran
+restoran2.restorani_kirjeldus()
+restoran2.ava_restoran()
+### kolmas restoran
+restoran3.restorani_kirjeldus()
+restoran3.ava_restoran()
+
