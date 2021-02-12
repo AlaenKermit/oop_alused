6f520450b3cec6316bf3919b5252bca91ff6b0fe
Allan Kerme <allan.kerme@khk.ee>
2021-02-12 10:36:16 +0200

    Ülesanne 6 lahendus

---
+++ Ülesanne 6/restoran.py
@@ -0,0 +1,15 @@
+from klass import *
+# loome 3 eksemplaari ja kasutame neid
+restoran1 = Restoran("BabyBack Ribs","Pizza 20€")
+restoran2 = Restoran("Timmu saiad","Juustumaitseline Baguette")
+restoran3 = Restoran("Burgerikuningas","Jalasalat")
+
+# kutsume eksemplaari meetodid tööle
+restoran1.restorani_kirjeldus()
+restoran1.ava_restoran()
+### teine restoran
+restoran2.restorani_kirjeldus()
+restoran2.ava_restoran()
+### kolmas restoran
+restoran3.restorani_kirjeldus()
+restoran3.ava_restoran()
