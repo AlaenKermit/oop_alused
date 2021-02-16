03b926fa7ca973e29b0102365d52e2f532810914
Allan Kerme <allan.kerme@khk.ee>
2021-02-16 11:44:05 +0200

    Ülesanne 11 poolik

--- Teema IV/Ülesanne 11/yl11klass.py
+++ Teema IV/Ülesanne 11/yl11klass.py
@@ -4,6 +4,8 @@ from random import randint
 class Inimene():
     def __init__(self):
         self.identifikatsioon = 0
+        global identifikatsioon
+        identifikatsioon = 0
 
     def assign_id(self):
         identifikatsioon += 1
