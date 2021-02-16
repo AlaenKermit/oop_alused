fd2fb9b6549d38b0a8e8c571789018a9dc7abfe9
Allan Kerme <allan.kerme@khk.ee>
2021-02-16 11:42:17 +0200

    Ülesanne 11 poolik

---
+++ Teema IV/Ülesanne 11/yl11klass.py
@@ -0,0 +1,19 @@
+from random import randint
+
+
+class Inimene():
+    def __init__(self):
+        self.identifikatsioon = 0
+
+    def assign_id(self):
+        identifikatsioon += 1
+
+    def info(self):
+        print("Sõduri ID on ", str(self.identifikatsioon))
+
+
+class Sodur(Inimene):
+    def __init__(self, armee):
+        super().__init__(armee)
+        self.armee = armee
+        self.armee = randint(1, 2)
---
+++ Teema IV/Ülesanne 11/yl11main.py
@@ -0,0 +1,25 @@
+from yl11klass import *
+
+armee1 = []
+armee2 = []
+counter = 20
+
+sodur1 = Inimene()
+sodur2 = Inimene()
+sodur3 = Inimene()
+sodur4 = Inimene()
+
+sodur1.assign_id()
+sodur2.assign_id()
+sodur3.assign_id()
+sodur4.assign_id()
+
+sodur1.info()
+sodur2.info()
+sodur3.info()
+sodur4.info()
+
+
+
+#for i in counter:
+#    print(1)
