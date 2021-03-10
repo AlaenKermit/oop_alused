8b21cc9a7ac99efca18f29e9fb7e867708975642
Allan Kerme <allan.kerme@khk.ee>
2021-03-10 13:23:54 +0200

    lAHENDUS

--- Teema VII/Ülesanne 16/yl16main.py
+++ Teema VII/Ülesanne 16/yl16main.py
@@ -1,22 +1,36 @@
-from yl16adminklass import *
+from yl16usertypeklass import *
+
+kasutaja = Kasutaja("Timmu", "Õunapuu","timmu.ounapuu","qwerty")
+administraator = Admin("Tõnu","Januvere","tonu.januvere","QWERTY")
+
+kasutaja.kirjelda_kasutajat("Workgroup")
+administraator.kirjelda_kasutajat("Workgroup")
+administraator.privileegid.show_privileges()
+
+
+
+
+
+
+
 # Teeme kolme kasutaja eksemplaarid
-kasutaja1 = Privileegid("Timmu","Õunapuu")
-kasutaja2 = Privileegid("Oliver","Mõttus")
-kasutaja3 = Privileegid("Joonatan","Kelder")
+#kasutaja1 = Privileegid("Timmu","Õunapuu")
+#kasutaja2 = Privileegid("Oliver","Mõttus")
+#kasutaja3 = Privileegid("Joonatan","Kelder")
 ### täpsustame kasutajate domeeni
-kasutaja1.set_privileeg("root")
-kasutaja2.set_privileeg("user")
-kasutaja3.set_privileeg("user")
+#kasutaja1.set_privileeg("root")
+#kasutaja2.set_privileeg("user")
+#kasutaja3.set_privileeg("user")
 ### täpsustame kasutajate privileegid
-kasutaja1.set_domain("Domeen")
-kasutaja2.set_domain("Domeen 2.0")
-kasutaja3.set_domain("WorkGroup 3.0")
+#kasutaja1.set_domain("Domeen")
+#kasutaja2.set_domain("Domeen 2.0")
+#kasutaja3.set_domain("WorkGroup 3.0")
 
 
-kasutaja1.kirjelda_kasutajat(kasutaja1.get_domain(),kasutaja1.get_privileeg())
-kasutaja2.kirjelda_kasutajat(kasutaja2.get_domain(),kasutaja2.get_privileeg())
-kasutaja3.kirjelda_kasutajat(kasutaja3.get_domain(),kasutaja3.get_privileeg())
+#kasutaja1.kirjelda_kasutajat(kasutaja1.get_domain(),kasutaja1.get_privileeg())
+#kasutaja2.kirjelda_kasutajat(kasutaja2.get_domain(),kasutaja2.get_privileeg())
+#kasutaja3.kirjelda_kasutajat(kasutaja3.get_domain(),kasutaja3.get_privileeg())
 
-kasutaja1.privileegid.show_privileges()
-kasutaja2.privileegid.show_privileges()
-kasutaja3.privileegid.show_privileges()
+#kasutaja1.privileegid.show_privileges()
+#kasutaja2.privileegid.show_privileges()
+#kasutaja3.privileegid.show_privileges()
