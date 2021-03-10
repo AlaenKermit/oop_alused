ed59033517cbeaa3a755f299dca246a5182ecce8
Allan Kerme <allan.kerme@khk.ee>
2021-03-10 13:33:18 +0200

    Moodulid - Klasside jaotamine moodulite vahel

---
+++ Teema IIX/kasutaja.py
@@ -0,0 +1,24 @@
+
+import users_and_privileges
+#administraator = Admin("Tõnu","Januvere","tonu.januvere","QWERTY")
+#administraator.privileegid.show_privileges()
+
+# A
+#from users_and_privileges import Admin
+
+#administraator = Admin("Tõnu","Januvere","tonu.januvere","QWERTY")
+#administraator.privileegid.show_privileges()
+
+
+#from users_and_privileges import *
+#administraator = Admin("Tõnu","Januvere","tonu.januvere","QWERTY")
+#administraator.privileegid.show_privileges()
+
+# B
+#import admin_and_privileges
+#administraator = admin_and_privileges.Admin("Tõnu","Januvere","tonu.januvere","QWERTY")
+#administraator.privileegid.show_privileges()
+
+from admin_and_privileges import Admin
+administraator = Admin("Tõnu","Januvere","tonu.januvere","QWERTY")
+administraator.privileegid.show_privileges()
