0d05d5e808494840502f4b90d0fe1cc8820701bc
Allan Kerme <allan.kerme@khk.ee>
2021-03-17 16:08:45 +0200

    "It Just Works" - Todd Howard

--- kool/oppimine.py
+++ kool/oppimine.py
@@ -1,14 +1,21 @@
+from random import randint
+
 class Andmed():
     """
     Allan Kerme oli siin!
     """
 
-    def __init__(self, *info):  # <------ |
-        self.info = list(info)  # ------> |
+    def __init__(self, info=None):
+        if(info is not None):# <------ |
+            self.info = list(info)  # ------> |
+        else:
+            self.info = []
 
     def __getitem__(self, index):  # väga kasulik!!!
         return self.info[index]
 
+    def add_info(self, info):
+        self.info.append(info)
 
 class Opilane():
     def __init__(self, nimi):
@@ -24,12 +31,14 @@ class Opilane():
             print("* " + teema)
         print("LÕPP")
 
-    def unutab(self, teema):
-        self.smarts.remove(teema)
+    def unutab(self):
+        topictoforget = randint(0, len(self.smarts))
+        self.smarts.remove(self.smarts[topictoforget])
 
 class Opetaja():
     def __init__(self, nimi):
         self.nimi = nimi
+
     def opetab(self, teema, *opilased):
         for i in opilased:
             i.opib(teema)
--- kool/tund.py
+++ kool/tund.py
@@ -1,6 +1,101 @@
 from oppimine import *
 
-aineteemad = Andmed("OOP põhimõtted", "Klassid ja Objektid", "Konstruktor")
+def choices():
+    print("Tunni programm")
+    print("1 - lisa aine teemad")
+    print("2 - trüki aine teemad")
+    print("3 - lisa õpilased klassi")
+    print("4 - näita klassis olevaid õpilasi")
+    print("5 - eemalda õpilane klassist")
+    print("6 - õpeta teema õpilastele")
+    print("7 - kontrolli õpilaste teadmised")
+    print("8 - lase õpilasele unustada teemat")
+    option = int(input("Vali sobilik tegevus: "))
+    return option
+
+def add_topic():
+    topics = Andmed()
+    while(True):
+        topic = input("Sisesta teema: ")
+        if(topic == ""):
+            break
+        topics.add_info(topic)
+    return topics
+
+def output_topics(lesson_topics):
+    if(len(lesson_topics.info) == 0):
+        print("Teemad pole sisestatud")
+    else:
+        print("Aine teemad on järgnevad:")
+        for topic in lesson_topics:
+            print("* " + topic)
+        print("---")
+
+def add_student(): # see on varajasem versioon mis töötab, aga on vale.
+    opilane = str(input("Sisesta õpilase nimi keda lisada klassi: "))
+    classroom.append(opilane)
+    show_classroom()
+
+''' selle peab korda saama, kuna ma pean õpilase tegema ju Opilane klassi objektiks
+def add_student():
+    opilane = str(input("Sisesta õpilase nimi keda lisada klassi: "))
+    print(opilane)
+    opilane = Opilane(opilane)
+    print(opilane)
+    classroom.append(opilane)
+    show_classroom()
+'''
+
+def show_classroom():
+    print("Klassi on lisatud hetkel järgnevad õpilased:")
+    counter = 0
+    for i in classroom:
+        counter += 1
+        print(counter, "- " + str(i))
+
+def remove_student():
+    print("Praegu on klassis järgnevad õpilased:")
+    show_classroom()
+    whotoremove = int(input("Sisestage number keda soovite klassist eemaldada: "))
+    correction = whotoremove - 1
+    print("Õpilane " + classroom[correction] + " on eemaldatud klassist.")
+    del classroom[correction]
+
+def teach_topic():
+    Anna.opetab(lesson_topics, classroom)
+
+def show_knowledge():
+    pass
+
+
+# TESTS START FROM HERE ON OUT
+
+lesson_topics = []
+classroom = []
+Anna = Opetaja
+while(True):
+    option = choices()
+    if(option == 1): # lisa teemad
+        lesson_topics = add_topic()
+    if(option == 2): # näitab kõik tunniteemad
+        output_topics(lesson_topics)
+    if(option == 3): # lisab õpilase klassi
+        add_student()
+    if(option == 4): # näitab klassi sisu
+        klassisisu = show_classroom()
+    if(option == 5): # eemaldab õpilase klassist
+        remove_student()
+    if(option == 6): # õpetab teema õpilastele
+        teach_topic()
+    if(option == 7): # kontrollib õpilaste teadmisi
+        pass
+    if(option == 8): # laseb õpilasel unustada teemat
+        pass
+
+
+
+'''
+aineteemad = Andmed()
 print("Aine teemad: ")
 for teema in aineteemad:
     print("* " + teema)
@@ -21,3 +116,4 @@ print("UNUSTAMISE TEST")
 print("Timmu unustab ära " + aineteemad[1])
 Timmu.unutab(aineteemad[1])
 Timmu.kirjeldus()
+'''
